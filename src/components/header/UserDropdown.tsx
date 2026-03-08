7ee9107e06b93ff2a3be9982815460ba8de3e79c
BagasAjiSaputra <bagaspdci555@gmail.com>
2026-03-08 21:38:39 +0700

    Fix pages

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -29,12 +29,12 @@ export default function UserDropdown() {
         className="flex items-center text-gray-700 dark:text-gray-400 dropdown-toggle"
       >
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <Image
+          {/* <Image
             width={44}
             height={44}
             src="/images/user/owner.jpg"
             alt="User"
-          />
+          /> */}
         </span>
 
         <span className="block mr-1 font-medium text-theme-sm">{session.user?.nama || session.user?.nama || "User"}</span>
